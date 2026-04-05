83fd6d30881798bab5c511164e4d7c9f34bdd85f
rowlow0gold-ops <rowlow0gold@gmail.com>
2026-04-06 01:33:27 +0900

    add swagger and sentry

--- src/main.js
+++ src/main.js
@@ -1,8 +1,24 @@
 import { createApp } from "vue";
+import * as Sentry from "@sentry/vue";
 import App from "./App.vue";
 import vuetify from "./plugins/vuetify";
 import router from "./router";
 import "./style.css";
 import pinia from "@/plugins/pinia";
 
-createApp(App).use(pinia).use(router).use(vuetify).mount("#app");
+const app = createApp(App);
+
+const sentryDsn = import.meta.env.VITE_SENTRY_DSN;
+if (sentryDsn) {
+  Sentry.init({
+    app,
+    dsn: sentryDsn,
+    integrations: [
+      Sentry.browserTracingIntegration({ router }),
+    ],
+    tracesSampleRate: 1.0,
+    environment: import.meta.env.MODE,
+  });
+}
+
+app.use(pinia).use(router).use(vuetify).mount("#app");
